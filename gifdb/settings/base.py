d2d3baf623feb70cd5e7685f43f19dc576427aeb
omniaura <omniaura5@gmail.com>
2013-08-22 10:19:15 -0700

    added Gif model, installed django-taggit

--- gifdb/settings/base.py
+++ gifdb/settings/base.py
@@ -135,6 +135,7 @@ DJANGO_APPS = (
 
 THIRD_PARTY_APPS = (
     'south',
+    'taggit'
 )
 
 LOCAL_APPS = (
--- search/models.py
+++ search/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib import admin
+from django.contrib.auth.models import User
+
+from taggit.managers import TaggableManager
+from taggit.models import TaggedItemBase
 
 # Create your models here.
+class Gif(models.Model):
+    url = models.CharField(max_length=200)
+    tags = TaggableManager()
+    date_added = models.DateField(auto_now_add=True)
+    user_added = models.ForeignKey(User)
+    nsfw = models.BooleanField()
+    
+    def __unicode__():
+        return self.url
+
+admin.site.register(Gif)
