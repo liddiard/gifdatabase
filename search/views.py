f02a3c7641124b5b6e18960ee6314fd90117f788
omniaura <omniaura5@gmail.com>
2013-08-27 13:32:58 -0700

    temporarily commented references to engine.py until updated for new schema

--- search/views.py
+++ search/views.py
@@ -1,11 +1,12 @@
 from django.views.generic.base import TemplateView
 from django.shortcuts import render_to_response
-from search import engine
+# from search import engine
 
 class SearchForm(TemplateView):
     template_name = "index.html"
 
 def SearchResults(request):
-    query = request.GET['q']
-    queryset = engine.query(query)
-    return render_to_response('results.html', {'results': queryset})
+#    query = request.GET['q']
+#    queryset = engine.query(query)
+#    return render_to_response('results.html', {'results': queryset})
+    pass
