6599c6fd6c598f600aa5674a43587f9f47347d7a
omniaura <omniaura5@gmail.com>
2013-08-27 17:52:23 -0700

    removed taggit from third party apps, reset south migrations

--- search/migrations/0001_initial.py
+++ search/migrations/0001_initial.py
@@ -11,17 +11,56 @@ class Migration(SchemaMigration):
         # Adding model 'Gif'
         db.create_table(u'search_gif', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('url', self.gf('django.db.models.fields.CharField')(max_length=200)),
-            ('date_added', self.gf('django.db.models.fields.DateField')(auto_now_add=True, blank=True)),
+            ('filename', self.gf('django.db.models.fields.CharField')(max_length=32)),
+            ('host', self.gf('django.db.models.fields.CharField')(max_length=2)),
+            ('date_added', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
             ('user_added', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
-            ('nsfw', self.gf('django.db.models.fields.BooleanField')(default=False)),
         ))
         db.send_create_signal(u'search', ['Gif'])
+
+        # Adding M2M table for field tags on 'Gif'
+        m2m_table_name = db.shorten_name(u'search_gif_tags')
+        db.create_table(m2m_table_name, (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('gif', models.ForeignKey(orm[u'search.gif'], null=False)),
+            ('taginstance', models.ForeignKey(orm[u'search.taginstance'], null=False))
+        ))
+        db.create_unique(m2m_table_name, ['gif_id', 'taginstance_id'])
+
+        # Adding model 'TagInstance'
+        db.create_table(u'search_taginstance', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('tag', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['search.Tag'])),
+            ('gif', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['search.Gif'])),
+            ('ups', self.gf('django.db.models.fields.IntegerField')()),
+            ('downs', self.gf('django.db.models.fields.IntegerField')()),
+            ('date_added', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('user_added', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
+        ))
+        db.send_create_signal(u'search', ['TagInstance'])
+
+        # Adding model 'Tag'
+        db.create_table(u'search_tag', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=50)),
+            ('date_added', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('user_added', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
+        ))
+        db.send_create_signal(u'search', ['Tag'])
 
 
     def backwards(self, orm):
         # Deleting model 'Gif'
         db.delete_table(u'search_gif')
+
+        # Removing M2M table for field tags on 'Gif'
+        db.delete_table(db.shorten_name(u'search_gif_tags'))
+
+        # Deleting model 'TagInstance'
+        db.delete_table(u'search_taginstance')
+
+        # Deleting model 'Tag'
+        db.delete_table(u'search_tag')
 
 
     models = {
@@ -62,25 +101,30 @@ class Migration(SchemaMigration):
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
         },
         u'search.gif': {
-            'Meta': {'object_name': 'Gif'},
-            'date_added': ('django.db.models.fields.DateField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'Meta': {'ordering': "['-date_added']", 'object_name': 'Gif'},
+            'date_added': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'filename': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
+            'host': ('django.db.models.fields.CharField', [], {'max_length': '2'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'nsfw': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'url': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'tags': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'+'", 'symmetrical': 'False', 'to': u"orm['search.TagInstance']"}),
             'user_added': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']"})
         },
-        u'taggit.tag': {
+        u'search.tag': {
             'Meta': {'object_name': 'Tag'},
+            'date_added': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'}),
-            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '100'})
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'user_added': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']"})
         },
-        u'taggit.taggeditem': {
-            'Meta': {'object_name': 'TaggedItem'},
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'taggit_taggeditem_tagged_items'", 'to': u"orm['contenttypes.ContentType']"}),
+        u'search.taginstance': {
+            'Meta': {'object_name': 'TagInstance'},
+            'date_added': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'downs': ('django.db.models.fields.IntegerField', [], {}),
+            'gif': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['search.Gif']"}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'object_id': ('django.db.models.fields.IntegerField', [], {'db_index': 'True'}),
-            'tag': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'taggit_taggeditem_items'", 'to': u"orm['taggit.Tag']"})
+            'tag': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['search.Tag']"}),
+            'ups': ('django.db.models.fields.IntegerField', [], {}),
+            'user_added': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']"})
         }
     }
 
