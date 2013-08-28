b9d79fccd60de18aaf06db9cdd75589dba800cd1
omniaura <omniaura5@gmail.com>
2013-08-28 13:12:53 -0700

    TagInstance admin display customization

--- search/models.py
+++ search/models.py
@@ -7,9 +7,33 @@ from taggit.models import TaggedItemBase, Tag
 
 class TagInstance(TaggedItemBase):
     content_object = models.ForeignKey('Gif', related_name=
-                                       "%(app_label)s_%(class)s_items")
+                                       "%(app_label)s_%(class)s_items",
+                                       verbose_name="on")
     ups = models.IntegerField(default=0)
     downs = models.IntegerField(default=0)
+    
+    def score(self):
+        try:
+            total = self.ups + self.downs
+            return round(self.ups / float(total), 2)
+        except ZeroDivisionError:
+            return 0.5
+    
+    def isBadTag(self):
+        threshold = 0.4
+        if self.score(self) < threshold:
+            return True
+        else:
+            return False
+    
+    def isVerified(self):
+        threshold = 0.6
+        min_votes = 2
+        total_votes = self.ups + self.downs
+        if self.score >= threshold and total_votes >= min_votes:
+            return True
+        else:
+            return False
     
     def __unicode__(self):
         data = {'host': self.content_object.host,
@@ -17,7 +41,11 @@ class TagInstance(TaggedItemBase):
                 'tag': self.tag,
                 'up': self.ups,
                 'down': self.downs}
-        return "[%(host)s-%(filename)s] %(up)s|%(down)s %(tag)s" % data               
+        return "%(up)s|%(down)s %(tag)s [%(host)s-%(filename)s]" % data
+
+class TagInstanceAdmin(admin.ModelAdmin):
+    fields = (('tag', 'content_object'), ('ups', 'downs'))
+    pass #list_display = ('score', 'isVerified') 
 
 class Gif(models.Model):
     filename = models.CharField(max_length=32)
@@ -34,5 +62,5 @@ class Gif(models.Model):
         return "[%s-%s]  %s" % (self.host, self.filename,
                             ', '.join(self.tags.names()))
 
-admin.site.register(TagInstance)
+admin.site.register(TagInstance, TagInstanceAdmin)
 admin.site.register(Gif)
