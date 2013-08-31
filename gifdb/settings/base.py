f3071b2cda06bf08b709e0eff83d728d99e649fa
omniaura <omniaura5@gmail.com>
2013-08-31 12:21:07 -0700

    changed base debug to true

--- gifdb/settings/base.py
+++ gifdb/settings/base.py
@@ -13,7 +13,7 @@ PROJECT_ROOT = here("..")
 # folder(s) we pass it starting at the parent directory of the current file.
 root = lambda * x: os.path.join(os.path.abspath(PROJECT_ROOT), *x)
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 if not DEBUG:
