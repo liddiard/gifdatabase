c2d5f311ecaa5ff8f9e3c1bcab8636e5da7d3698
omniaura <omniaura5@gmail.com>
2013-08-30 19:50:37 -0700

    reordered models

--- search/models.py
+++ search/models.py
@@ -11,56 +11,6 @@ from taggit.forms import TagWidget
 
 DEFAULT_USER_ID = 1
 
-class TagInstance(TaggedItemBase):
-    content_object = models.ForeignKey('Gif', related_name=
-                                       "%(app_label)s_%(class)s_items",
-                                       verbose_name="on")
-    ups = models.PositiveIntegerField(default=0)
-    downs = models.PositiveIntegerField(default=0)
-    date_added = models.DateTimeField(auto_now_add=True)
-    user_added = models.ForeignKey(User, default=DEFAULT_USER_ID)
-    
-    def score(self):
-        try:
-            total = self.ups + self.downs
-            return round(self.ups / float(total), 2)
-        except ZeroDivisionError:
-            return 0.5
-    
-    def isBadTag(self):
-        threshold = 0.4
-        if self.score(self) < threshold:
-            return True
-        else:
-            return False
-    
-    def isVerified(self):
-        threshold = 0.6
-        min_votes = 2
-        total_votes = self.ups + self.downs
-        if self.score >= threshold and total_votes >= min_votes:
-            return True
-        else:
-            return False
-    isVerified.boolean = True
-    isVerified.short_description = "v"
-    
-    def __unicode__(self):
-        data = {'host': self.content_object.host,
-                'filename': self.content_object.filename,
-                'tag': self.tag,
-                'up': self.ups,
-                'down': self.downs}
-        return "%(up)s|%(down)s %(tag)s [%(host)s-%(filename)s]" % data
-
-class TagInstanceAdmin(admin.ModelAdmin):
-    list_display = ('isVerified', 'tag', 'ups', 'downs', 'content_object',
-                    'user_added', 'date_added')
-    list_display_links = ('tag',)
-    fields = (('tag', 'content_object'), ('ups', 'downs'),
-              ('user_added', 'date_added'))
-    readonly_fields = ('date_added', 'tag', 'content_object')
-
 class Gif(models.Model):
     filename = models.CharField(max_length=32)
     HOST_CHOICES = (('ig', 'imgur'), ('mi', 'minus'))
@@ -116,6 +66,48 @@ class Gif(models.Model):
     class Meta:
         ordering = ["-date_added"]
 
+class TagInstance(TaggedItemBase):
+    content_object = models.ForeignKey('Gif', related_name=
+                                       "%(app_label)s_%(class)s_items",
+                                       verbose_name="on")
+    ups = models.PositiveIntegerField(default=0)
+    downs = models.PositiveIntegerField(default=0)
+    date_added = models.DateTimeField(auto_now_add=True)
+    user_added = models.ForeignKey(User, default=DEFAULT_USER_ID)
+    
+    def score(self):
+        try:
+            total = self.ups + self.downs
+            return round(self.ups / float(total), 2)
+        except ZeroDivisionError:
+            return 0.5
+    
+    def isBadTag(self):
+        threshold = 0.4
+        if self.score(self) < threshold:
+            return True
+        else:
+            return False
+    
+    def isVerified(self):
+        threshold = 0.6
+        min_votes = 2
+        total_votes = self.ups + self.downs
+        if self.score >= threshold and total_votes >= min_votes:
+            return True
+        else:
+            return False
+    isVerified.boolean = True
+    isVerified.short_description = "v"
+    
+    def __unicode__(self):
+        data = {'host': self.content_object.host,
+                'filename': self.content_object.filename,
+                'tag': self.tag,
+                'up': self.ups,
+                'down': self.downs}
+        return "%(up)s|%(down)s %(tag)s [%(host)s-%(filename)s]" % data
+
 class GifAdmin(admin.ModelAdmin):
     def displayGif(self, obj):
         return u'<img src="%s"/>' % obj.getUrl()
@@ -131,6 +123,14 @@ class GifAdmin(admin.ModelAdmin):
     formfield_overrides = {
         TaggableManager: {'widget': TagWidget(attrs={'size':'400'})},
     }
+
+class TagInstanceAdmin(admin.ModelAdmin):
+    list_display = ('isVerified', 'tag', 'ups', 'downs', 'content_object',
+                    'user_added', 'date_added')
+    list_display_links = ('tag',)
+    fields = (('tag', 'content_object'), ('ups', 'downs'),
+              ('user_added', 'date_added'))
+    readonly_fields = ('date_added', 'tag', 'content_object')
 
 admin.site.register(Gif, GifAdmin)
 admin.site.register(TagInstance, TagInstanceAdmin)
