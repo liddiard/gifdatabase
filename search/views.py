0bbe3a6cc014d45e27377d00477332d758b750b3
omniaura <omniaura5@gmail.com>
2013-09-01 01:30:29 -0700

    basic user login

--- search/views.py
+++ search/views.py
@@ -1,12 +1,22 @@
 from django.views.generic.base import TemplateView
 from django.shortcuts import render_to_response
+from django.contrib.auth import authenticate
 from search import engine
 from gifdb.settings.base import S3_URL
 
 class SearchForm(TemplateView):
     template_name = "front.html"
 
-def SearchResults(request):
+def login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        render_to_response("Welcome to the cool kids club.")
+    else:
+        render_to_response("Whatcha think you're doin round these parts, son?")
+
+def searchResults(request):
     query = request.GET['q']
     queryset = engine.query(query)
     return render_to_response('results.html', {'results': queryset, 'S3_URL': S3_URL})
