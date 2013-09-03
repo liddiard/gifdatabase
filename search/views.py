8804a26f116459d91f4aa4060e2159f07afbc32c
omniaura <omniaura5@gmail.com>
2013-09-03 14:56:25 -0700

    profile favorites implemented (template vars need work)

--- search/views.py
+++ search/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render_to_response, redirect
+from django.shortcuts import render_to_response, redirect, get_object_or_404
 from django.contrib.auth import (authenticate, login as django_login,
                                  logout as django_logout)
 from django.template import RequestContext
 from django.contrib.auth.forms import AuthenticationForm
 from search import engine
 from gifdb.settings.base import S3_URL
+from search.models import User, UserFavorite
 
 def frontPage(request):
     return render_to_response('front.html', {'S3_URL': S3_URL},
@@ -35,8 +36,15 @@ def logout(request):
     return redirect('main')
 
 def profile(request, username):
+    user_profile = get_object_or_404(User, username=username)
+    starred_recent = UserFavorite.objects.filter(user=user_profile)\
+                                                 .order_by('-date_favorited')[:5]
+    added_recent = None
+    tagged_recent = None
+    
     return render_to_response('profile.html',
-                              {'username': username, 'S3_URL': S3_URL},
+                              {'username': user_profile, 
+                               'starred_recent': starred_recent,'S3_URL': S3_URL},
                               context_instance=RequestContext(request))
 
 def profileStarred(request, username):
