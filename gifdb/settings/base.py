ffa1c59bfc2e1638e7cb4fd53ca6c0cafa5985ad
omniaura <omniaura5@gmail.com>
2013-09-26 02:00:32 -0700

    switched to unminified colorup.js to add img.crossOrigin

--- gifdb/settings/base.py
+++ gifdb/settings/base.py
@@ -114,7 +114,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #'corsheaders.middleware.CorsMiddleware' # TODO: remove if unneeded
+    'corsheaders.middleware.CorsMiddleware' # TODO: remove if unneeded
 )
 
 ROOT_URLCONF = 'gifdb.urls'
