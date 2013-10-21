aeb2ca28508f82e288225ea3103df8ed0f389eb6
omniaura <omniaura5@gmail.com>
2013-10-21 04:04:32 -0700

    removed character keys from slimbox next/prev/close control

--- search/static/js/taglight.js
+++ search/static/js/taglight.js
@@ -67,9 +67,9 @@ votes = {};
 			imageFadeDuration: 0,			// Duration of the image fade-in animation (in milliseconds)
 			captionAnimationDuration: 0,		// Duration of the caption animation (in milliseconds)
 			counterText: "gif {x} of {y}",	// Translate or change as you wish, or set it to false to disable counter text for image groups
-			closeKeys: [27, 88, 67],		// Array of keycodes to close Slimbox, default: Esc (27), 'x' (88), 'c' (67)
-			previousKeys: [37, 80],			// Array of keycodes to navigate to the previous image, default: Left arrow (37), 'p' (80)
-			nextKeys: [39, 78]			// Array of keycodes to navigate to the next image, default: Right arrow (39), 'n' (78)
+			closeKeys: [27],		// Array of keycodes to close Slimbox, default: Esc (27), 'x' (88), 'c' (67)
+			previousKeys: [37],			// Array of keycodes to navigate to the previous image, default: Left arrow (37), 'p' (80)
+			nextKeys: [39]			// Array of keycodes to navigate to the next image, default: Right arrow (39), 'n' (78)
 		}, _options);
 
 		// The function is called for a single image, with URL and Title as first two arguments
