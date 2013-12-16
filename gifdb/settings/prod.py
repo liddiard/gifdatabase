2d88bc5028ffb3df5ec889ebdd791a16e2df8b67
omniaura <omniaura5@gmail.com>
2013-12-15 16:07:47 -0800

    changed production email hostname

--- gifdb/settings/prod.py
+++ gifdb/settings/prod.py
@@ -13,7 +13,7 @@ DATABASES = {
 REGISTRATION_OPEN = True
 
 # email settings
-EMAIL_HOST = 'smtp.mandrill.com'
+EMAIL_HOST = 'smtp.mandrillapp.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = get_env_variable('MANDRILL_USERNAME')
 EMAIL_HOST_PASSWORD = get_env_variable('MANDRILL_APIKEY')
