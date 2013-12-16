f2453211d98aec334b03557a7e7add19bded1b46
omniaura <omniaura5@gmail.com>
2013-12-15 16:05:32 -0800

    removed ssl for production email sending

--- gifdb/settings/prod.py
+++ gifdb/settings/prod.py
@@ -14,7 +14,6 @@ REGISTRATION_OPEN = True
 
 # email settings
 EMAIL_HOST = 'smtp.mandrill.com'
-EMAIL_PORT = 465
+EMAIL_PORT = 587
 EMAIL_HOST_USER = get_env_variable('MANDRILL_USERNAME')
 EMAIL_HOST_PASSWORD = get_env_variable('MANDRILL_APIKEY')
-EMAIL_USE_SSL = True
