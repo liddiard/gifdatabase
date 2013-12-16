b8ce3ae56eae22d82200bd002d3e92d69db4b48d
omniaura <omniaura5@gmail.com>
2013-12-15 21:08:26 -0800

    added email sender name, improved language in email templates

--- gifdb/settings/prod.py
+++ gifdb/settings/prod.py
@@ -13,8 +13,8 @@ DATABASES = {
 REGISTRATION_OPEN = True
 
 # email settings
-EMAIL_HOST = 'smtp.mandrillapp.com'
+EMAIL_HOST = "smtp.mandrillapp.com"
 EMAIL_PORT = 587
 EMAIL_HOST_USER = get_env_variable('MANDRILL_USERNAME')
 EMAIL_HOST_PASSWORD = get_env_variable('MANDRILL_APIKEY')
-DEFAULT_FROM_EMAIL = 'no-reply@gifdatabase.com'
+DEFAULT_FROM_EMAIL = "GIFdatabase <no-reply@gifdatabase.com>"
