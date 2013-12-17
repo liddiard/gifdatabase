b950a03101b0e1212704b115021d8ea1b26c7ac1
omniaura <omniaura5@gmail.com>
2013-12-17 00:10:29 -0800

    added *.gifdatabase.com to ALLOWED_HOSTS

--- gifdb/settings/base.py
+++ gifdb/settings/base.py
@@ -41,7 +41,7 @@ MANAGERS = ADMINS
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['localhost', '.herokuapp.com', 'gifdatabase.com']
+ALLOWED_HOSTS = ['localhost', '.herokuapp.com', '.gifdatabase.com']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
