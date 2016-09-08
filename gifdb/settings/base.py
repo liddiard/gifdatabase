96a7046221cd0266048168b9d112f5bc74bc3040
omniaura <omniaura5@gmail.com>
2016-09-07 17:50:27 -0700

    moved templates to app dir, update template settings

--- gifdb/settings/base.py
+++ gifdb/settings/base.py
@@ -23,7 +23,6 @@ def get_env_variable(var_name):
         raise ImproperlyConfigured(error_msg)
 
 DEBUG = False
-TEMPLATE_DEBUG = DEBUG
 
 AWS_STORAGE_BUCKET_NAME = "gifdatabase"
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
@@ -107,13 +106,6 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # NOT cryptographically secure
 OBFUSCATION_KEY = int(os.environ['OBFUSCATION_KEY'], 16)
 
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
-)
-
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -128,13 +120,6 @@ ROOT_URLCONF = 'gifdb.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'gifdb.wsgi.application'
-
-TEMPLATE_DIRS = (
-    root("templates"),
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
 
 DJANGO_APPS = (
     'django.contrib.auth',
@@ -151,7 +136,6 @@ DJANGO_APPS = (
 )
 
 THIRD_PARTY_APPS = (
-    'south',
     'taggit',
     'storages',
     'registration'
@@ -166,22 +150,23 @@ INSTALLED_APPS = THIRD_PARTY_APPS + DJANGO_APPS + LOCAL_APPS
 # 'registration' settings
 ACCOUNT_ACTIVATION_DAYS = 7
 
-DJANGO_CONTEXT_PROCESSORS = (
-    "django.contrib.auth.context_processors.auth",
-    "django.core.context_processors.debug",
-    "django.core.context_processors.i18n",
-    "django.core.context_processors.media",
-    "django.core.context_processors.static",
-    "django.core.context_processors.tz",
-    "django.core.context_processors.request",
-    "django.contrib.messages.context_processors.messages"
-)
-
-USER_CONTEXT_PROCESSORS = ()
-
-TEMPLATE_CONTEXT_PROCESSORS = (DJANGO_CONTEXT_PROCESSORS + 
-
-                               USER_CONTEXT_PROCESSORS)
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
